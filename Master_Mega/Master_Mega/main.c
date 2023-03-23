1e06fd2ddef6091d6885822f18dafaf7f79551f0
Vili Raunola <raunolavili@gmail.com>
2023-03-23 14:00:13 +0200

    Added Microchip Studio projects for master and slave

---
+++ Master_Mega/Master_Mega/main.c
@@ -0,0 +1,18 @@
+/*
+ * Master_Mega.c
+ *
+ * Created: 23/03/2023 13:56:38
+ * Author : Group 07
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ Slave_Uno/Slave_Uno/main.c
@@ -0,0 +1,18 @@
+/*
+ * Slave_Uno.c
+ *
+ * Created: 23/03/2023 13:59:02
+ * Author : Group 07
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
